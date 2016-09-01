d97f2d58ff520d73a42d0a634a378133d2b514a6
Rabea Gleissner <rabeagleissner@gmail.com>
2016-09-01 12:04:19 +0100

    adds improved subtract method back

--- calculator.rb
+++ calculator.rb
@@ -4,6 +4,10 @@ class Calculator
     firstNumber + secondNumber
   end
 
+  def subtract(firstNumber, secondNumber)
+    firstNumber - 100
+  end
+
   def multiply(firstNumber, secondNumber)
     firstNumber * secondNumber
   end
